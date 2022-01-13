43eee6497138d3ebf8cd35e6665309872c74e273
gaetanLBT <gaetan.lebreton@live.fr>
2022-01-14 00:57:35 +0100

    phase 1 MS API REST datas + connection mongoDB + .env

--- MS_backend_Data/index.js
+++ MS_backend_Data/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import {} from "dotenv/config.js";
+
 
 import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
+
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true},() => {
+    console.log('connected to Database');
+});
 
 app.use(bodyParser.json());
 
